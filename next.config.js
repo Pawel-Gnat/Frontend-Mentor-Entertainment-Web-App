88ee3449bc430c109e2c7354efc6fa4f5bcd64b2
Paweł <pawel_gnat@o2.pl>
2023-09-04 13:03:20 +0200

    refactor: api pass

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-
-	env: {
-		mongodb_username: 'pawel',
-		mongodb_password: 'rjK3WI0uy7kHEEx5',
-		mongodb_clustername: 'cluster0',
-		mongodb_database: 'entertainment-web-app',
-	},
 }
 
 module.exports = nextConfig
